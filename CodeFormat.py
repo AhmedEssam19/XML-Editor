c236e38f9769fa45126b7e96c406028359e6cd49
MustafaElAntrawy <mustafaelantrawy@gmail.com>
2021-07-12 08:54:11 +0200

    Adding fix_error function

--- CodeFormat.py
+++ CodeFormat.py
@@ -11,7 +11,6 @@ def mark_error(code: str):
     while i < len(code):
         if code[i] == '\n':
             line_number += 1
-            i += 1
         if code[i] == '<':
             i += 1
             temp = str()
@@ -57,8 +56,65 @@ def mark_error(code: str):
         else:
             str_in_tag += code[i]
             i += 1
+    while not tag.is_empty():
+        error_list.append(line_number)
+        tag_line.pop()
+        tag.pop()
 
     return error_list
+
+
+def fix_error(code: str):
+    if len(mark_error(code)) == 0:
+        return code
+    new_code = str()
+    tag = Stack()
+    i = 0
+    str_in_tag = "\t"
+    while i < len(code):
+        if code[i] == '<':
+            i += 1
+            temp = str()
+            if code[i] == '/':
+                i += 1
+                while code[i] != '>':
+                    temp += code[i]
+                    i += 1
+                i += 1
+                if not tag.is_empty() and temp == tag.top():
+                    str_in_tag = "\t"
+                    new_code += f"</{temp}>"
+                    tag.pop()
+                else:
+                    if not str_in_tag.isspace():
+                        new_code += f"</{tag.top()}>"
+                        tag.pop()
+                        str_in_tag = "\t"
+                    else:
+                        while not tag.is_empty() and temp != tag.top():
+                            new_code += f"</{tag.top()}>"
+                            tag.pop()
+                        if not tag.is_empty() and temp == tag.top():
+                            new_code += f"</{tag.top()}>"
+                            tag.pop()
+            else:
+                if not str_in_tag.isspace():
+                    new_code += f"</{tag.top()}>"
+                    tag.pop()
+                while code[i] != '>':
+                    temp += code[i]
+                    i += 1
+                i += 1
+                new_code += f"<{temp}>"
+                tag.push(temp)
+        else:
+            str_in_tag += code[i]
+            new_code += code[i]
+            i += 1
+    while not tag.is_empty():
+        new_code += f"</{tag.top()}>"
+        tag.pop()
+    return new_code
 
 
 #tial
@@ -70,9 +126,9 @@ s = "<users>" \
     "\n\t\t\t<follower>" \
     "\n\t\t\t\t<name>l</id>" \
     "\n\t\t\t</follower>" \
-    "\n\t\t</followers>" \
-    "\n\t</user>" \
-    "\n</users>"
+    "\n\t\t" \
+    "\n\t" \
+    "\n"
 index = 0
 tt = ""
 while index < len(s):
@@ -81,6 +137,8 @@ while index < len(s):
     index += 1
 
 
-print(tt)
+#print(tt)
 
-print(mark_error(s))
+print(fix_error(tt))
+print(mark_error(fix_error(tt)))
+
