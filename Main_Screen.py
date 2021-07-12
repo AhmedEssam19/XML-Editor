c500647cb52f05fcc637c386d6b296edf4686856
mostafa-elsharnoby <mostafaelsharnobi@gmail.com>
2021-07-12 12:06:38 +0200

    Add Menus

--- Main_Screen.py
+++ Main_Screen.py
@@ -1,16 +1,59 @@
 import sys
+import xmltodict
+import json
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QMenuBar, QMainWindow, QAction
+from PyQt5.QtGui import QIcon
 from PyQt5.uic import loadUi
 
+from PyQt5.QtWidgets import QWidget, QLineEdit, QPlainTextEdit, QVBoxLayout
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QColor, QRegExpValidator, QSyntaxHighlighter, QTextCharFormat
 
-class MainWindow(QDialog):
+class SyntaxHighlighter(QSyntaxHighlighter):
+    def __init__(self, parent):
+        super(SyntaxHighlighter, self).__init__(parent)
+        self._highlight_lines = dict()
+
+    def highlight_line(self, line, fmt):
+        if isinstance(line, int) and line >= 0 and isinstance(fmt, QTextCharFormat):
+            self._highlight_lines[line] = fmt
+            tb = self.document().findBlockByLineNumber(line)
+            self.rehighlightBlock(tb)
+
+    def clear_highlight(self):
+        self._highlight_lines = dict()
+        self.rehighlight()
+
+    def highlightBlock(self, text):
+        line = self.currentBlock().blockNumber()
+        fmt = self._highlight_lines.get(line)
+        if fmt is not None:
+            self.setFormat(0, len(text), fmt)
+
+
+class MainWindow(QMainWindow, QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("gui.ui", self)
-        self.Browse.clicked.connect(self.browsefiles)
+        #self.Browse.clicked.connect(self.XML2JSON)
+        self.create_menu()
+
+        self.lines = [5,8,9]
+        self._highlighter = SyntaxHighlighter(self.textEdit.document())
+
+        self.show()
+
+    def onTextChanged(self):
+        fmt = QTextCharFormat()
+        fmt.setBackground(QColor("yellow"))
+        self._highlighter.clear_highlight()
+        for i in range(0,len(self.lines)):
+            self._highlighter.highlight_line(self.lines[i], fmt)
+
 
     def browsefiles(self):
+        # actionNew_File.triggered.connect(self.browsefiles)
         fname = QFileDialog.getOpenFileName(self, 'open file', 'D:', 'XML files (*.xml)')
         self.filename.setText(fname[0])
         path = fname[0]
@@ -18,6 +61,177 @@ class MainWindow(QDialog):
             data = f.read()
             self.textEdit.setText(data)
             print(data)
+        return data
+
+    def XML2JSON(self):
+        data = '''<?xml version="1.0"?>
+<catalog>
+   <book id="bk101">
+      <author>Gambardella, Matthew</author>
+      <title>XML Developer's Guide</title>
+      <genre>Computer</genre>
+      <price>44.95</price>
+      <publish_date>2000-10-01</publish_date>
+      <description>An in-depth look at creating applications 
+      with XML.</description>
+   </book>
+   <book id="bk102">
+      <author>Ralls, Kim</author>
+      <title>Midnight Rain</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2000-12-16</publish_date>
+      <description>A former architect battles corporate zombies, 
+      an evil sorceress, and her own childhood to become queen 
+      of the world.</description>
+   </book>
+   <book id="bk103">
+      <author>Corets, Eva</author>
+      <title>Maeve Ascendant</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2000-11-17</publish_date>
+      <description>After the collapse of a nanotechnology 
+      society in England, the young survivors lay the 
+      foundation for a new society.</description>
+   </book>
+   <book id="bk104">
+      <author>Corets, Eva</author>
+      <title>Oberon's Legacy</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2001-03-10</publish_date>
+      <description>In post-apocalypse England, the mysterious 
+      agent known only as Oberon helps to create a new life 
+      for the inhabitants of London. Sequel to Maeve 
+      Ascendant.</description>
+   </book>
+   <book id="bk105">
+      <author>Corets, Eva</author>
+      <title>The Sundered Grail</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2001-09-10</publish_date>
+      <description>The two daughters of Maeve, half-sisters, 
+      battle one another for control of England. Sequel to 
+      Oberon's Legacy.</description>
+   </book>
+   <book id="bk106">
+      <author>Randall, Cynthia</author>
+      <title>Lover Birds</title>
+      <genre>Romance</genre>
+      <price>4.95</price>
+      <publish_date>2000-09-02</publish_date>
+      <description>When Carla meets Paul at an ornithology 
+      conference, tempers fly as feathers get ruffled.</description>
+   </book>
+   <book id="bk107">
+      <author>Thurman, Paula</author>
+      <title>Splish Splash</title>
+      <genre>Romance</genre>
+      <price>4.95</price>
+      <publish_date>2000-11-02</publish_date>
+      <description>A deep sea diver finds true love twenty 
+      thousand leagues beneath the sea.</description>
+   </book>
+   <book id="bk108">
+      <author>Knorr, Stefan</author>
+      <title>Creepy Crawlies</title>
+      <genre>Horror</genre>
+      <price>4.95</price>
+      <publish_date>2000-12-06</publish_date>
+      <description>An anthology of horror stories about roaches,
+      centipedes, scorpions  and other insects.</description>
+   </book>
+   <book id="bk109">
+      <author>Kress, Peter</author>
+      <title>Paradox Lost</title>
+      <genre>Science Fiction</genre>
+      <price>6.95</price>
+      <publish_date>2000-11-02</publish_date>
+      <description>After an inadvertant trip through a Heisenberg
+      Uncertainty Device, James Salway discovers the problems 
+      of being quantum.</description>
+   </book>
+   <book id="bk110">
+      <author>O'Brien, Tim</author>
+      <title>Microsoft .NET: The Programming Bible</title>
+      <genre>Computer</genre>
+      <price>36.95</price>
+      <publish_date>2000-12-09</publish_date>
+      <description>Microsoft's .NET initiative is explored in 
+      detail in this deep programmer's reference.</description>
+   </book>
+   <book id="bk111">
+      <author>O'Brien, Tim</author>
+      <title>MSXML3: A Comprehensive Guide</title>
+      <genre>Computer</genre>
+      <price>36.95</price>
+      <publish_date>2000-12-01</publish_date>
+      <description>The Microsoft MSXML3 parser is covered in 
+      detail, with attention to XML DOM interfaces, XSLT processing, 
+      SAX and more.</description>
+   </book>
+   <book id="bk112">
+      <author>Galos, Mike</author>
+      <title>Visual Studio 7: A Comprehensive Guide</title>
+      <genre>Computer</genre>
+      <price>49.95</price>
+      <publish_date>2001-04-16</publish_date>
+      <description>Microsoft Visual Studio 7 is explored in depth,
+      looking at how Visual Basic, Visual C++, C#, and ASP+ are 
+      integrated into a comprehensive development 
+      environment.</description>
+   </book>
+</catalog>'''
+        my_dict = xmltodict.parse(data)
+        json_data = json.dumps(my_dict)
+        text = json_data.encode('ascii').decode('unicode-escape')
+        self.textEdit.setText(text)
+        print(json_data)
+
+    def create_menu(self):
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu("File")
+        viewMenu = mainMenu.addMenu("View")
+        editMenu = mainMenu.addMenu("Edit")
+        searchMenu = mainMenu.addMenu("Font")
+        helpMenu = mainMenu.addMenu("Help")
+
+        openAction = QAction(QIcon('open.png'), "Open", self)
+        openAction.setShortcut("Ctrl+O")
+
+        saveAction = QAction(QIcon('save.png'), "Save", self)
+        saveAction.setShortcut("Ctrl+S")
+
+        exitAction = QAction(QIcon('exit.png'), "Exit", self)
+        exitAction.setShortcut("Ctrl+X")
+
+        view_errorAction = QAction(QIcon('exit.png'), "View Error", self)
+        view_errorAction.setShortcut("Ctrl+E")
+
+        saveAction.triggered.connect(self.save_file)
+        exitAction.triggered.connect(self.exit_app)
+        openAction.triggered.connect(self.browsefiles)
+
+        view_errorAction.triggered.connect(self.onTextChanged)
+
+        fileMenu.addAction(openAction)
+        fileMenu.addAction(saveAction)
+        fileMenu.addAction(exitAction)
+
+        viewMenu.addAction(view_errorAction)
+
+    def save_file(self):
+        name, _ = QFileDialog.getSaveFileName(self, 'Save File', options=QFileDialog.DontUseNativeDialog)
+        file = open(name, 'w')
+        text = self.textEdit.toPlainText()
+        file.write(text)
+        file.close()
+
+    def exit_app(self):
+        self.close()
+
 
 app = QApplication(sys.argv)
 mainwindow = MainWindow()
